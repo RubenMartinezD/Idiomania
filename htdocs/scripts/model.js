d46daf8fa3948d401377cd978f2f90595ac0c943
Rubén <rmartinezdominguez@danielcastelao.org>
2022-05-18 13:45:04 +0200

    conexión conseguida

--- htdocs/scripts/model.js
+++ htdocs/scripts/model.js
@@ -3,15 +3,18 @@ class Model {
 
     }
     conectarDB() {
-        var apellidos = prompt("Introduce los apellidos a buscar");
+        var id_frase = prompt("Introduce el ID de la frase a buscar");
         $.ajax({
-            data: { "apellidos": apellidos },
-            url: 'main.php',
+            data: { "id_frase": id_frase },
+            url: 'scripts/obtenerFrase.php',
             type: 'get',
             success: function(response) {
                 var respuesta = JSON.parse(response);
-                $("#respuesta").html(" Apellidos: " + respuesta.apellidos);
-
+                for (var i = 0; i < respuesta.length; i++) {
+                    $("#respuesta").html("id_frase: " + respuesta[i].id_frase + " Contenido: " +
+                        respuesta[i].contenido);
+                    console.log(respuesta)
+                }
             }
         });
     }
