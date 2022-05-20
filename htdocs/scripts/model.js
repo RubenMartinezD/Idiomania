670ca3f3be05b0c706e5363cf5eaf6e7a85966fb
Rubén <rmartinezdominguez@danielcastelao.org>
2022-05-20 11:55:56 +0200

    frase obtenida al azar

--- htdocs/scripts/model.js
+++ htdocs/scripts/model.js
@@ -1,21 +1,24 @@
 class Model {
-    constructor() {
-
-    }
-    conectarDB() {
-        var id_frase = prompt("Introduce el ID de la frase a buscar");
+    constructor() { }
+    /**
+     * Método para obtener una frase al azar de la base de datos y mostrarla por pantalla
+     * @returns boolean true
+     */
+    obtenerFrase() {
+        var id_frase = Math.floor(Math.random() * 120) + 1;
         $.ajax({
             data: { "id_frase": id_frase },
-            url: 'scripts/obtenerFrase.php',
+            url: 'php/obtenerFrase.php',
             type: 'get',
-            success: function(response) {
+            success: function (response) {
                 var respuesta = JSON.parse(response);
                 for (var i = 0; i < respuesta.length; i++) {
                     $("#respuesta").html("id_frase: " + respuesta[i].id_frase + " Contenido: " +
                         respuesta[i].contenido);
-                    console.log(respuesta)
+                    console.log(respuesta);
                 }
             }
         });
+        return true;
     }
 }
