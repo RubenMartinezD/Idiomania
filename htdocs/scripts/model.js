87db299342d228c368ab2be2b93fece253d3ab17
Rubén <rmartinezdominguez@danielcastelao.org>
2022-05-22 13:57:00 +0200

    posible avance

--- htdocs/scripts/model.js
+++ htdocs/scripts/model.js
@@ -1,16 +1,16 @@
 class Model {
-    constructor() { }
-    /**
-     * Método para obtener una frase al azar de la base de datos y mostrarla por pantalla
-     * @returns boolean true
-     */
+    constructor() {}
+        /**
+         * Método para obtener una frase al azar de la base de datos y mostrarla por pantalla
+         * @returns boolean true
+         */
     obtenerFrase() {
         var id_frase = Math.floor(Math.random() * 120) + 1;
         $.ajax({
             data: { "id_frase": id_frase },
             url: 'php/obtenerFrase.php',
             type: 'get',
-            success: function (response) {
+            success: function(response) {
                 var respuesta = JSON.parse(response);
                 for (var i = 0; i < respuesta.length; i++) {
                     $("#respuesta").html("id_frase: " + respuesta[i].id_frase + " Contenido: " +
@@ -20,5 +20,45 @@ class Model {
             }
         });
         return true;
+    }
+
+    traducirFrase() {
+        /** 
+                const projectId = 'future-sunrise-350010';
+
+                // Imports the Google Cloud client library
+                const { Translate } = require('@google-cloud/translate').v2;
+
+                // Instantiates a client
+                const translate = new Translate({ projectId });
+
+                async function quickStart() {
+                    // The text to translate
+                    const text = 'Hello, world!';
+
+                    // The target language
+                    const target = 'ru';
+
+                    // Translates some text into Russian
+                    const [translation] = await translate.translate(text, target);
+                    console.log(`Text: ${text}`);
+                    console.log(`Translation: ${translation}`);
+                }
+
+                quickStart();
+
+            }
+        */
+        $.ajax({
+            data: { "id_frase": id_frase },
+            url: 'php/traducirFrase.php',
+            type: 'get',
+            success: function(response) {
+                var respuesta = JSON.parse(response);
+                console.log(respuesta);
+            }
+        });
+
+
     }
 }
