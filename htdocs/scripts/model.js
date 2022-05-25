4072f04200ffdd58df534701502aff994e4d36e2
RubenMartinezD <92438338+RubenMartinezD@users.noreply.github.com>
2022-05-25 20:16:20 +0200

    Prueba api (#12)
    * posible avance
    
    * intento 2
    
    * intento con nueva API
    
    * pruebas
    
    * Implementación de Axios

--- htdocs/scripts/model.js
+++ htdocs/scripts/model.js
@@ -1,16 +1,16 @@
 class Model {
-    constructor() { }
-    /**
-     * Método para obtener una frase al azar de la base de datos y mostrarla por pantalla
-     * @returns boolean true
-     */
+    constructor() {}
+        /**
+         * Método para obtener una frase al azar de la base de datos y mostrarla por pantalla
+         * @returns boolean true
+         */
     obtenerFrase() {
         var id_frase = Math.floor(Math.random() * 120) + 1;
         $.ajax({
             data: { "id_frase": id_frase },
             url: 'php/obtenerFrase.php',
             type: 'get',
-            success: function (response) {
+            success: function(response) {
                 var respuesta = JSON.parse(response);
                 for (var i = 0; i < respuesta.length; i++) {
                     $("#respuesta").html("id_frase: " + respuesta[i].id_frase + " Contenido: " +
@@ -20,5 +20,27 @@ class Model {
             }
         });
         return true;
+    }
+
+    traducirFrase() {
+        async function traduccion() {
+
+            const response = await axios
+                .get("https://api-free.deepl.com/v2/translate", {
+                    params: {
+                        auth_key: "a57a350c-3be2-fa4f-3318-a5e5e8adf8c8:fx",
+                        text: "Adios mundo",
+                        target_lang: "DE"
+                    },
+                    proxy: {
+                        host: "localhost",
+                        port: 8080
+                    }
+                });
+            return response;
+        }
+        traduccion().then(function(resultado) {
+            console.log(JSON.parse(resultado.request.response).translations[0].text);
+        })
     }
 }
