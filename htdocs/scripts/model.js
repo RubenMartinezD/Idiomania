26f3b255efe02f125f7cf502e0e516d115301f70
RubenMartinezD <92438338+RubenMartinezD@users.noreply.github.com>
2022-05-26 12:56:53 +0200

    Seguimiento_proyecto_Rubén_semana_2 (#17)
    * Seguimiento_proyecto_Rubén_semana_1 (#7) (#8)
    
    * Probando conexión
    
    * conexión conseguida
    
    * Diagrama de flujo v0.5
    
    * .
    
    * frase obtenida al azar (#10)
    
    * Prueba api (#12)
    
    * posible avance
    
    * intento 2
    
    * intento con nueva API
    
    * pruebas
    
    * Implementación de Axios
    
    * base de datos mejorada (#15)
    
    * Objeto jugador (#16)
    
    * posible avance
    
    * intento 2
    
    * intento con nueva API
    
    * develop
    
    * jugador en creación
    
    * v0.2
    
    * incorporación de objeto cronometro en progreso
    
    * intento de cuenta atrás en MVC

--- htdocs/scripts/model.js
+++ htdocs/scripts/model.js
@@ -1,21 +1,50 @@
 class Model {
     constructor() {
-
-    }
-    conectarDB() {
-        var id_frase = prompt("Introduce el ID de la frase a buscar");
+            cuentita = new cuentaAtras();
+        }
+        /**
+         * Método para obtener una frase al azar de la base de datos y mostrarla por pantalla
+         * @returns boolean true
+         */
+    obtenerFrase() {
+        var id_frase = Math.floor(Math.random() * 120) + 1;
         $.ajax({
             data: { "id_frase": id_frase },
-            url: 'scripts/obtenerFrase.php',
+            url: 'php/obtenerFrase.php',
             type: 'get',
             success: function(response) {
                 var respuesta = JSON.parse(response);
                 for (var i = 0; i < respuesta.length; i++) {
                     $("#respuesta").html("id_frase: " + respuesta[i].id_frase + " Contenido: " +
                         respuesta[i].contenido);
-                    console.log(respuesta)
+                    console.log(respuesta);
                 }
             }
         });
+        return true;
+    }
+    nuevaCuenta() {
+        this.cuentita.comenzarCuenta()
+    }
+    traducirFrase() {
+        async function traduccion() {
+
+            const response = await axios
+                .get("https://api-free.deepl.com/v2/translate", {
+                    params: {
+                        auth_key: "a57a350c-3be2-fa4f-3318-a5e5e8adf8c8:fx",
+                        text: "Adios mundo",
+                        target_lang: "DE"
+                    },
+                    proxy: {
+                        host: "localhost",
+                        port: 8080
+                    }
+                });
+            return response;
+        }
+        traduccion().then(function(resultado) {
+            console.log(JSON.parse(resultado.request.response).translations[0].text);
+        })
     }
 }
