7418504fb78bb22719cff82f677afd9e6904e7d7
RubenMartinezD <92438338+RubenMartinezD@users.noreply.github.com>
2022-06-02 12:11:56 +0200

    Menu idiomas (#22)
    * commit hasta ahora
    
    * corregido randomizarFrase()
    
    * corregido randomizarFrase()
    
    * recoger mas idiomas, distintos del correcto
    
    * creadas cuatro opciones randomizadas con la frase
    
    * modificado index

--- htdocs/scripts/model.js
+++ htdocs/scripts/model.js
@@ -2,7 +2,7 @@ class Model {
     constructor() {
         this.cuentita = new CuentaAtras()
         this.jugador = new Player()
-        this.respuesta_correcta = 0
+        this.lugar_respuesta_correcta = 0
     }
 
     nuevoJugador() {
@@ -22,16 +22,18 @@ class Model {
                 data: { "id_frase": id_frase },
                 url: 'php/obtenerFrase.php',
                 type: 'get',
+                async: false,
                 success: function(response) {
                     var respuesta = JSON.parse(response);
                     for (var i = 0; i < respuesta.length; i++) {
-                        $("#respuesta").html("id_frase: " + respuesta[i].id_frase + " Contenido: " +
-                            respuesta[i].contenido);
+                        $("#frase").html(respuesta[i].contenido);
                         console.log(respuesta);
+                        return true;
                     }
+
                 }
             });
-            return true;
+
         }
         /**
          * Método para obtener un idioma al azar de la base de datos y mostrarla por pantalla
@@ -55,11 +57,52 @@ class Model {
                     for (var i = 0; i < idioma.length; i++) {
                         $("#idioma_traducido").html(idioma[i].nombre_idioma)
                         $("#codigo_idioma_correcto").html(idioma[i].codigo_idioma)
+                        $("#id_idioma_correcto").html(idioma[i].id_idioma)
                     }
                     console.log(idioma);
-                    return idioma[i].codigo_idioma;
+                    return true;
                 }
             });
+
+        }
+        /**
+         * Método para obtener
+         * @param {*} id_idioma 
+         * @param {*} numero_slot 
+         */
+    obtenerIdioma2(id_idioma, numero_slot) {
+        /**
+         * Extraer con ajax la frase correspondiente e imprimirla en las etiquetas html ocultas idioma_traducido y codigo_idioma_correcto
+         */
+        $.ajax({
+            data: { "id_idioma": id_idioma },
+            url: 'php/obtenerIdioma.php',
+            type: 'get',
+            async: false,
+            success: function(response) {
+                var idioma = JSON.parse(response);
+                for (let i = 0; i < idioma.length; i++) {
+                    $("#nombre_idiomaextra" + numero_slot).html(idioma[i].nombre_idioma)
+                }
+                console.log(idioma);
+                return true;
+            }
+        });
+
+    }
+
+
+    recogerMasIdiomas() {
+            var idioma_preprohibido = document.getElementById("id_idioma_correcto");
+            var idioma_prohibido = idioma_preprohibido.textContent;
+            console.log("Idioma recogido:" + idioma_prohibido);
+            var idioma_extra = [];
+            for (let i = 0; i < 3; i++) {
+                do { idioma_extra[i] = Math.floor(Math.random() * 25) + 1; }
+                while (idioma_extra[i] == idioma_prohibido)
+                console.log("Idioma extra" + i + ":" + idioma_extra[i])
+                this.obtenerIdioma2(idioma_extra[i], i)
+            }
 
         }
         /**
@@ -103,19 +146,88 @@ class Model {
         /**
          * Función que actúa como un pack 3 en 1 para entregar una frase en un idioma al azar
          */
+    colocarBotones() {
+        var lugar_respuesta_correcta = Math.floor(Math.random() * 4) + 1;
+        var respuesta_incorrecta1 = 0
+        var respuesta_incorrecta2 = 0
+        var respuesta_incorrecta3 = 0
+        var boton1 = document.getElementById("Boton 1");
+        var boton2 = document.getElementById("Boton 2");
+        var boton3 = document.getElementById("Boton 3");
+        var boton4 = document.getElementById("Boton 4");
+        var idiomatraducido = document.getElementById("idioma_traducido").textContent;
+        var idiomaextra0 = document.getElementById("nombre_idiomaextra0").textContent;
+        var idiomaextra1 = document.getElementById("nombre_idiomaextra1").textContent;
+        var idiomaextra2 = document.getElementById("nombre_idiomaextra2").textContent;
+        do { respuesta_incorrecta1 = Math.floor(Math.random() * 4) + 1; }
+        while (respuesta_incorrecta1 == lugar_respuesta_correcta);
+        do { respuesta_incorrecta2 = Math.floor(Math.random() * 4) + 1; }
+        while (respuesta_incorrecta2 == lugar_respuesta_correcta || respuesta_incorrecta2 == respuesta_incorrecta1);
+        do { respuesta_incorrecta3 = Math.floor(Math.random() * 4) + 1; }
+        while (respuesta_incorrecta3 == lugar_respuesta_correcta || respuesta_incorrecta3 == respuesta_incorrecta1 || respuesta_incorrecta3 == respuesta_incorrecta2);
+        switch (lugar_respuesta_correcta) {
+            case 1:
+                boton1.textContent = idiomatraducido
+                break;
+            case 2:
+                boton2.textContent = idiomatraducido
+                break;
+            case 3:
+                boton3.textContent = idiomatraducido
+                break;
+            case 4:
+                boton4.textContent = idiomatraducido
+                break;
+        }
+        switch (respuesta_incorrecta1) {
+            case 1:
+                boton1.textContent = idiomaextra0
+                break;
+            case 2:
+                boton2.textContent = idiomaextra0
+                break;
+            case 3:
+                boton3.textContent = idiomaextra0
+                break;
+            case 4:
+                boton4.textContent = idiomaextra0
+                break;
+        }
+        switch (respuesta_incorrecta2) {
+            case 1:
+                boton1.textContent = idiomaextra1
+                break;
+            case 2:
+                boton2.textContent = idiomaextra1
+                break;
+            case 3:
+                boton3.textContent = idiomaextra1
+                break;
+            case 4:
+                boton4.textContent = idiomaextra1
+                break;
+        }
+        switch (respuesta_incorrecta3) {
+            case 1:
+                boton1.textContent = idiomaextra2
+                break;
+            case 2:
+                boton2.textContent = idiomaextra2
+                break;
+            case 3:
+                boton3.textContent = idiomaextra2
+                break;
+            case 4:
+                boton4.textContent = idiomaextra2
+                break;
+        }
+    }
     randomizarFrase() {
         this.obtenerFrase()
         this.obtenerIdioma()
         this.traducirFrase()
-        this.respuesta_correcta = Math.floor(Math.random() * 4) + 1;
-    }
-
-    iniciarJuego() {
-        var nomheroe = comprobarNombre();
-        heroe = new Heroe(nomheroe, 2, 20, 5, 2, 40, 25)
-        $("#NOMH").html(heroe.getnombre)
-        $("#NOME").html(enemigo.getnombre)
-        sessionStorage.setItem("NOMH", nomheroe)
+        this.recogerMasIdiomas()
+        this.colocarBotones()
     }
 
 
